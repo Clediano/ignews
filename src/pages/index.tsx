2741b2ecdd5533bd389b477494e9317b5ed18e37
Clediano Estefenon <clediano.estefenon@evoluum.com.br>
2022-04-01 14:31:15 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <div>
+        Hello <span>World</span>
+      </div>
+    </>
+  )
+}
